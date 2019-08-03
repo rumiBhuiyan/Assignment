a6d1a29ee26f6cf2fe6ba7683ab502f3005f850a
Fatema Akter Rumi <rumiapple28@gmail.com>
2019-08-03 18:40:07 +0600

    break simulator commit

---
+++ BreakSimulator/BreakSimulator/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace BreakSimulator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int testCases = int.Parse(Console.ReadLine());
+
+            while (testCases != 0)
+            {
+                string number = Console.ReadLine();
+                testCases--;
+                string[] inputs = number.Split(' ');
+                double velocity = double.Parse(inputs[0]);
+                double time = double.Parse(inputs[1]);
+                double deceleration = (-velocity / time);
+
+                Console.WriteLine(deceleration.ToString("0.00"));
+            }
+            Console.Read();
+        }
+    }
+}
