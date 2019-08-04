91d5a839dc2a7c53c5cbba8124255377b643b163
Fatema Akter Rumi <rumiapple28@gmail.com>
2019-08-05 02:58:30 +0600

    commit AssemblyTest project

---
+++ AssemblyTest/AssemblyTest/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AssemblyTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello");
+            MessageBox.Show("Hello world");
+        }
+    }
+}
