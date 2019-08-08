48e55c85f8ee2e3b3b4a679a086adb2b7fac4117
Fatema Akter Rumi <rumiapple28@gmail.com>
2019-08-08 14:27:12 +0600

    numberSquareCube commit

---
+++ AssignmentOne/NumberSquareCube/NumberSquareCube/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace NumberSquareCube
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            int i;
+            Console.WriteLine("number       square      cube");
+            for ( i = 0; i <= 10; i++)
+            {
+                Console.WriteLine($"{i}             {i * i}             {i * i * i}");
+            }
+            Console.Read();
+        }
+    }
+}
